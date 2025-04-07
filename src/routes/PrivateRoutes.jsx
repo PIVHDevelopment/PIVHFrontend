a9e2be6f950b5f2f822f93f4458ee2419df59db3
RumitVhits <rumitgajjar.vhits@gmail.com>
2025-04-07 17:14:15 +0530

    sign in spinner show

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import DataService from "../utils/DataService";
 
 const PrivateRoutes = ({ children }) => {
   const userData = JSON.parse(sessionStorage.getItem("pi_user_data"));
-//   DataService.defaults.headers.common.auth = userData?.uid;
+  //   DataService.defaults.headers.common.auth = userData?.uid;
 
   return userData?.uid ? <>{children}</> : <Navigate to="/" replace={true} />;
 };
