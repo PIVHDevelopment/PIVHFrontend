49e06ff28505bc9a44915160315cdf31d0993335
kanaReact <kanatukadiya.vhits@gmail.com>
2025-04-10 15:16:21 +0530

    Changes

--- src/utils/DataService.js
+++ src/utils/DataService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const DataService = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: 'https://pocketpitest.com/api/',
   timeout: 20000,
   withCredentials: true,
 });
