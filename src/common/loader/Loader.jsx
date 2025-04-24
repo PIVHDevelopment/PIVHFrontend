63649fa26c6ea3c6bad7c8b80b32c744ed640c3f
saloni-suthar <saloni.vhits@gmail.com>
2025-04-24 11:14:46 +0530

    Create loader page

---
+++ src/common/loader/Loader.jsx
@@ -0,0 +1,24 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import Index from '../../container/Index'
+
+const Loader = () => {
+      return (
+            <>
+
+                  <div className="app-container p-20-0">
+                        <div className="loader-main">
+                              <Box className='loader-details'>
+                                    <div class="loader-ring"></div>
+                                    <Box class="loader-inner">
+                                          <img src={Index.pocketPi} alt="Loading..." className="loader-img" />
+                                    </Box>
+
+                              </Box>
+                        </div>
+                  </div>
+            </>
+      )
+}
+
+export default Loader
