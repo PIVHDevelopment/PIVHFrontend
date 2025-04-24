afd4f86cba1cbe4d344b93e3ce30ec23801a5373
rajkumar patidar <rajkumarpatidar.vhits@gmail.com>
2025-04-24 18:31:40 +0530

    Create receive page

--- src/container/receive/Receive.jsx
+++ src/container/receive/Receive.jsx
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
 import Index from "../Index";
-
+import { QRCodeCanvas } from "qrcode.react";
 function Receive() {
   const userData = JSON.parse(sessionStorage.getItem("pi_user_data"));
   const navigate = Index.useNavigate();
   const location = Index.useLocation();
   let typeTxn = location?.state?.typeTxn;
+  
     const [copied, setCopied] = useState(false);
   console.log({location});
+  console.log({userData});
 
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(typeTxn == "business" ? businessUserName : userData?.userName);
+    navigator.clipboard.writeText(typeTxn == "business" ? userData?.businessUserName : userData?.userName);
     setCopied(true);
     setTimeout(() => setCopied(false), 3000);
   };
-  
+
   return (
     <div className="app-container">
       <header className="receive-center">
@@ -28,35 +30,36 @@ function Receive() {
         <div className="header-right"></div>
       </header>
 
-        <div className="wallet-id">
+          <div className="wallet-id">
               <span id="walletAddress">
                 {typeTxn == "business" ?  userData?.businessUserName : userData?.userName }
               </span>
               <button className="copy-btn" onClick={handleCopy}>
                 {copied ? <span>✓</span> : <img src={Index.copy} alt="Copy" />}
               </button>
-            </div>
-
+              </div>
+          
       <div className="qr-section">
         <div className="qr-code">
           {/* <img src={Index.qrCode} alt="QR Code" /> */}
+          <QRCodeCanvas value={typeTxn == "business" ? userData?.businessUserName : userData?.userName} size={200} />
         </div>
       </div>
 
-      <div className="action-buttons">
+      {/* <div className="action-buttons">
         <button className="circle-btn">
           <span className="icon">
             <img src={Index.copyBtn} alt="Copy" />
           </span>
           <span>Copy</span>
         </button>
-        <button className="circle-btn">
+        <button className="circle-btn" >
           <span className="icon">
             <img src={Index.downloadImg} alt="Download" />
           </span>
           <span>Download</span>
         </button>
-      </div>
+      </div> */}
 
       <div className="receive-btn-share">
         <button className="secondary-btn share-btn">
