a003fd0ca2c44ebd469878ebb68e3ea2b3e7f769
rajkumar patidar <rajkumarpatidar.vhits@gmail.com>
2025-04-30 10:56:21 +0530

    Remove console

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+{
+  console.log = () => { };
+  console.error = () => { };
+  console.debug = () => { };
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
