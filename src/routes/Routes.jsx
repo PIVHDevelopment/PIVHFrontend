3d2aa7ae893cedeaa508b849dfe577bbf6156f42
rajkumar patidar <rajkumarpatidar.vhits@gmail.com>
2025-05-06 14:12:30 +0530

    Take pull and resolved conflicts

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -29,11 +29,8 @@ import Loader from "../common/loader/Loader";
 import Subscription from "../container/subscription/Subscription";
 import QrScanner from "../container/send/QrScanner";
 import WalletAddressBook from "../container/walletAddressBook/WalletAddressBook";
-<<<<<<< HEAD
 import PaymentRequest from "../container/paymentRequest/PaymentRequest";
-=======
 import TransactionSuccess from "../container/transactionSuccess/TransactionSuccess";
->>>>>>> ab574be5db18ce2c68ed1b92ef41f3fb189cd9cf
 
 function ErrorBoundary() {
   const error = useRouteError(); // Assuming useRouteError is defined somewhere
