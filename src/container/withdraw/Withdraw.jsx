a5c27ed23db252ed0b6c0a2c8b8e96c6be05b939
rajkumar patidar <rajkumarpatidar.vhits@gmail.com>
2025-05-08 12:25:51 +0530

    Request payment time show success animation page

--- src/container/withdraw/Withdraw.jsx
+++ src/container/withdraw/Withdraw.jsx
@@ -45,7 +45,7 @@ function Withdraw() {
       const res = await Index.DataService.post(Index.Api.WITHDRAW, paymentData);
       setButtonLoader(false);
       if (res?.data?.status === 200) {
-        Index.toasterSuccess(res?.data?.message);
+        // Index.toasterSuccess(res?.data?.message);
         navigate("/transaction-success", {
           state: { isBusiness: typeTxn == "business" ? true : false },
         });
