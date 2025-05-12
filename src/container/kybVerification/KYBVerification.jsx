b63c6cb1dc613978092905bdc2e9868b3677e75a
rajkumar patidar <rajkumarpatidar.vhits@gmail.com>
2025-05-12 15:47:54 +0530

    Work on kyb verfication

--- src/container/kybVerification/KYBVerification.jsx
+++ src/container/kybVerification/KYBVerification.jsx
@@ -9,16 +9,18 @@ import {
   Autocomplete,
   TextField,
 } from "@mui/material";
-import ClearIcon from '@mui/icons-material/Clear';
+import ClearIcon from "@mui/icons-material/Clear";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import countries from "./countries.json";
 
 export default function KYBVerification() {
- const userData = JSON.parse(sessionStorage.getItem("pi_user_data"));
- const navigate = Index.useNavigate();
- const { t } = Index.useTranslation();
+  const userData = JSON.parse(sessionStorage.getItem("pi_user_data"));
+  const navigate = Index.useNavigate();
+  const { t } = Index.useTranslation();
+  const language = localStorage.getItem("language");
+   const [loading, setLoading] = React.useState(false);
 
- const formik = useFormik({
+  const formik = useFormik({
     initialValues: {
       country: { name: "India" } || null,
       frontDocument: null,
@@ -27,20 +29,18 @@ export default function KYBVerification() {
     },
     validationSchema: Index.addKybVerificationSchema,
     onSubmit: async (values) => {
-        console.log({values});
-        
-        const formData = new FormData();
-  
-        formData.append("userId", userData?._id);
-        formData.append("country", values?.country?.name);
-      
-        // Append documents depending on country
-        if (values.country.name === "India") {
-          formData.append("documents", values.frontDocument);
-          formData.append("documents", values.backDocument);
-        } else {
-          formData.append("documents", values.singleDocument);
-        }
+    setLoading(true);
+      const formData = new FormData();
+      formData.append("userId", userData?._id);
+      formData.append("country", values?.country?.name);
+
+      // Append documents depending on country
+      if (values.country.name === "India") {
+        formData.append("documents", values.frontDocument);
+        formData.append("documents", values.backDocument);
+      } else {
+        formData.append("documents", values.singleDocument);
+      }
 
       try {
         const res = await Index.DataService.post(
@@ -48,18 +48,19 @@ export default function KYBVerification() {
           formData
         );
         if (res?.data?.status === 200 || res?.data?.status === 201) {
-          Index.toasterSuccess(res?.data?.message);
+          Index.toasterSuccess(res?.data?.message?.[language]);
           formik.resetForm();
           navigate("/check-kyb-verification");
         }
       } catch (error) {
         Index.toasterError(
-          error?.response?.data?.message
+          error?.response?.data?.message?.[language] || t("AnUnexpectedErrorOccurred")
         );
+      } finally {
+        setLoading(false);
       }
     },
   });
-
 
   const FilePreview = ({ file }) => {
     if (!file) return null;
@@ -75,21 +76,20 @@ export default function KYBVerification() {
         />
       );
     }
-  
+
     return (
       <img
         src={fileURL}
         alt="Document Preview"
-       className="user-upload-profile-img"
+        className="user-upload-profile-img"
       />
     );
   };
-  
 
   return (
     <div className="app-container">
       <header className="receive-center">
-        <button className="back-btn" onClick={() => navigate(-1)}>
+        <button className="back-btn" onClick={() =>  navigate("/check-kyb-verification")}>
           <img src={Index.back} alt="Back" />
         </button>
         <div className="app-icon">
@@ -131,24 +131,23 @@ export default function KYBVerification() {
                         <TextField
                           {...params}
                           name="country"
-                        //   error={
-                        //     formik.touched.country &&
-                        //     Boolean(formik.errors.country)
-                        //   }
-                        //   helperText={
-                        //     formik.touched.country && formik.errors.country
-                        //   }
+                          //   error={
+                          //     formik.touched.country &&
+                          //     Boolean(formik.errors.country)
+                          //   }
+                          //   helperText={
+                          //     formik.touched.country && formik.errors.country
+                          //   }
                           className="dropdown-select"
                           placeholder="Select Country"
                         />
                       )}
                     />
-                     {formik.touched.country &&
-                            formik.errors.country && (
-                              <p color="error" className="input-error">
-                                {formik.errors.country}
-                              </p>
-                            )}
+                    {formik.touched.country && formik.errors.country && (
+                      <p color="error" className="input-error">
+                        {formik.errors.country}
+                      </p>
+                    )}
                   </div>
                 </Index.Box>
 
@@ -167,13 +166,28 @@ export default function KYBVerification() {
                             component="label"
                             className="user-file-upload-btn"
                           >
-                              {formik.values.frontDocument ?
-                              <img
-                              src={URL.createObjectURL(formik.values.frontDocument)}
-                              alt="Front Document Preview"
-                             className="user-upload-profile-img"
-                            /> :
-                            <CloudUploadIcon className="user-upload-icon-img" /> }
+                            {formik.values.frontDocument ? (
+                              <>
+                                <FilePreview
+                                  file={formik.values.frontDocument}
+                                />
+                                <Box className="clear-icon">
+                                  <ClearIcon
+                                    fontSize="small"
+                                    onClick={(e) => {
+                                      e.stopPropagation();
+                                      formik.setFieldValue(
+                                        "frontDocument",
+                                        null
+                                      );
+                                    }}
+                                  />
+                                </Box>
+                              </>
+                            ) : (
+                              <CloudUploadIcon className="user-upload-icon-img" />
+                            )}
+
                             <input
                               hidden
                               accept="image/*,.pdf"
@@ -208,14 +222,27 @@ export default function KYBVerification() {
                             component="label"
                             className="user-file-upload-btn"
                           >
-                         {formik.values.backDocument ?
-                              <img
-                              src={URL.createObjectURL(formik.values.backDocument)}
-                              alt="Front Document Preview"
-                             className="user-upload-profile-img"
-                            /> :
-                            <CloudUploadIcon className="user-upload-icon-img" />
-                            }
+                            {formik.values.backDocument ? (
+                              <>
+                                <FilePreview
+                                  file={formik.values.backDocument}
+                                />
+                                <Box className="clear-icon">
+                                  <ClearIcon
+                                    fontSize="small"
+                                    onClick={(e) => {
+                                      e.stopPropagation();
+                                      formik.setFieldValue(
+                                        "backDocument",
+                                        null
+                                      );
+                                    }}
+                                  />
+                                </Box>
+                              </>
+                            ) : (
+                              <CloudUploadIcon className="user-upload-icon-img" />
+                            )}
                             <input
                               hidden
                               accept="image/*,.pdf"
@@ -251,17 +278,27 @@ export default function KYBVerification() {
                           component="label"
                           className="user-file-upload-btn"
                         >
-                        {formik.values.singleDocument ? (
-                          <>
-                         <FilePreview file={formik.values.singleDocument} />
-                          <ClearIcon  onClick={(e) => {
-                            e.stopPropagation();
-                           formik.setFieldValue("singleDocument", null);
-                           }}/>
-                          </>
-                       ) : (
-                         <CloudUploadIcon className="user-upload-icon-img" />
-                       )}
+                          {formik.values.singleDocument ? (
+                            <>
+                              <FilePreview
+                                file={formik.values.singleDocument}
+                              />
+                              <Box className="clear-icon">
+                                <ClearIcon
+                                  fontSize="small"
+                                  onClick={(e) => {
+                                    e.stopPropagation();
+                                    formik.setFieldValue(
+                                      "singleDocument",
+                                      null
+                                    );
+                                  }}
+                                />
+                              </Box>
+                            </>
+                          ) : (
+                            <CloudUploadIcon className="user-upload-icon-img" />
+                          )}
 
                           <input
                             hidden
@@ -291,6 +328,7 @@ export default function KYBVerification() {
             <button
               className="action-btn full-width button-top-space"
               type="submit"
+              disabled={loading}
             >
               Submit
             </button>
